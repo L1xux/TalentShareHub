2d8577601b83910967a963c7e1ce1a7bdd8d6f44
Steven Lee <mr938363@gmail.com>
2023-10-05 21:10:48 +0900

    fix #2: change category icon url & border in home

--- src/app/_assets/category/category.ts
+++ src/app/_assets/category/category.ts
@@ -3,19 +3,19 @@ const category = [
   {
     id: 1,
     name: "언어",
-    icon: "/image/icon/language.png",
+    icon: `${process.env.NEXT_PUBLIC_TEST_URL}/image/icon/language.png`,
   },
   // mathematics
   {
     id: 2,
     name: "수학",
-    icon: "/image/icon/mathematics.png",
+    icon: `/image/icon/mathematics.png`,
   },
   // develop
   {
     id: 3,
     name: "개발",
-    icon: "/image/icon/develop.png",
+    icon: `${process.env.NEXT_PUBLIC_TEST_URL}/image/icon/develop.png`,
   },
 ];
 
